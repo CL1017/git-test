206f766e7cf547ba7e12c92fd9b65521e7a9d274
cl <cl@atguigu.com>
2023-08-25 11:48:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @auther 陈龙
+ */
+@SuppressWarnings({"all"})
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
